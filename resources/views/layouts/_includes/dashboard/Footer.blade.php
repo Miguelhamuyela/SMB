d87b523d81c26149e09b4b712c71a5c9dd9e18d6
Your Name <tecnicopaulo@outlook.pt>
2022-01-26 16:14:15 +0100

    dsc

--- resources/views/layouts/_includes/dashboard/Footer.blade.php
+++ resources/views/layouts/_includes/dashboard/Footer.blade.php
@@ -73,19 +73,24 @@
 
 
 
-
-<!-- container-scroller -->
 <!-- plugins:js -->
 <script src="/dashboard/vendors/js/vendor.bundle.base.js"></script>
 <script src="/dashboard/vendors/js/vendor.bundle.addons.js"></script>
 <!-- endinject -->
+
+
 <!-- Plugin js for this page-->
 <!-- End plugin js for this page-->
+
+
+
 <!-- inject:js -->
 <script src="/dashboard/js/shared/off-canvas.js"></script>
 <script src="/dashboard/js/shared/misc.js"></script>
 <!-- endinject -->
+
 <!-- Custom js for this page-->
 <script src="/dashboard/js/demo_1/dashboard.js"></script>
+
 </body>
 </html>
