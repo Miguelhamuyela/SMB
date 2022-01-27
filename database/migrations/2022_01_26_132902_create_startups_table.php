43cd8670475d407595c057b2e754a1871aa7377b
hamuyela45@hotmail.com <hamuyela45@hotmail.com>
2022-01-27 09:59:24 +0100

    fg

--- database/migrations/2022_01_26_132902_create_startups_table.php
+++ database/migrations/2022_01_26_132902_create_startups_table.php
@@ -14,31 +14,19 @@ class CreateStartupsTable extends Migration
     public function up()
     {
         Schema::create('startups', function (Blueprint $table) {
-<<<<<<< HEAD:database/migrations/2022_01_26_132902_create_startups_table.php
-
-=======
             $table->id();
->>>>>>> 51b3f8c6e3fc0ab900dc0f4cb832e89dea8b2f68:database/migrations/2022_01_26_113700_create_startups_table.php
             $table->string('name');
             $table->string('roomName');
             $table->string('site');
             $table->string('email');
             $table->string('tel');
             $table->string('nif');
-<<<<<<< HEAD:database/migrations/2022_01_26_132902_create_startups_table.php
-
-            $table->foreign('fk_Scheldules_id')->references('id')->on('scheldules')->onDelete('CASCADE')->onUpgrade('CASCADE');
-
-           
-=======
             $table->unsignedBigInteger('fk_Scheldules_id');
             $table->foreign('fk_Scheldules_id')->references('id')->on('scheldules');
-            
+
             $table->unsignedBigInteger('fk_Payments_id');
             $table->foreign('fk_Payments_id')->references('id')->on('payments');
->>>>>>> 51b3f8c6e3fc0ab900dc0f4cb832e89dea8b2f68:database/migrations/2022_01_26_113700_create_startups_table.php
 
-            $table->id();
             $table->softDeletes();
             $table->timestamps();
 
--- database/migrations/2022_01_26_135854_create_members_table.php
+++ database/migrations/2022_01_26_135854_create_members_table.php
@@ -14,9 +14,6 @@ class CreateMembersTable extends Migration
     public function up()
     {
         Schema::create('members', function (Blueprint $table) {
-<<<<<<< HEAD:database/migrations/2022_01_26_135854_create_members_table.php
-
-=======
             $table->id();
             $table->string('occupation');
             $table->string('email');
@@ -25,22 +22,10 @@ class CreateMembersTable extends Migration
             $table->string('nif');
             $table->unsignedBigInteger('fk_startups_id');
             $table->foreign('fk_startups_id')->references('id')->on('startups');
-            
-            $table->softDeletes();
-            $table->timestamps();
->>>>>>> 51b3f8c6e3fc0ab900dc0f4cb832e89dea8b2f68:database/migrations/2022_01_26_115718_create_members_table.php
-
-            $table->string('occupation');
-            $table->string('email');
-            $table->string('tel');
-            $table->string('name');
-            $table->string('tel');
-            $table->string('nif');
-
-            $table->foreign('fk_startups_id')->references('id')->on('startups')->onDelete('CASCADE')->onUpgrade('CASCADE');
 
             $table->softDeletes();
             $table->timestamps();
+
         });
     }
 
