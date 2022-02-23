4e2973057042b1f011e773c79603d32dc4477673
Your Name <tecnicopaulo@outlook.pt>
2022-02-23 09:56:18 +0100

    jbn

--- resources/views/pdf/credential/employees/index.blade.php
+++ resources/views/pdf/credential/employees/index.blade.php
@@ -1,1 +1,23 @@
-hello work
+<!DOCTYPE html>
+<html lang="pt-pt">
+
+<head>
+    <meta charset="UTF-8">
+
+    <title>Passe de Funcionário</title>
+    <style>
+
+    </style>
+</head>
+
+<body style="background-image: url('dashboard/images/idcard/index.jpg');
+background-position: top left;
+             background-repeat: no-repeat;
+             background-image-resize: 2;
+             background-image-resolution: from-image;
+">
+
+
+</body>
+
+</html>
