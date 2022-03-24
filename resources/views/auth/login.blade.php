487e09a469e5efa4d8b56c3ee98662543832a766
Your Name <tecnicopaulo@outlook.pt>
2022-03-24 13:57:17 +0100

    jn

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -17,10 +17,10 @@
                                 </a>
                 
                             <!-- Session Status -->
-                            <x-auth-session-status class="mb-4" :status="session('status')" />
+                            <x-auth-session-status class="mb-4 alert alert-success" :status="session('status')" />
                 
                             <!-- Validation Errors -->
-                            <x-auth-validation-errors class="mb-4" :errors="$errors" />
+                            <x-auth-validation-errors class="mb-4 alert alert-danger" :errors="$errors" />
                             <form method="POST" class="mt-5"  action="{{ route('login') }}">
                                @csrf
                                 <!-- Email Address -->
@@ -88,67 +88,3 @@
 
 @endsection
 
-
-
-{{-- <div class="auto-form-wrapper d-flex align-items-center justify-content-center flex-column">
-    <x-slot name="logo">
-        <a href="{{ route('admin.home') }}">
-
-            <img src="/dashboard/logotipo/logo.png" alt="Logo" width="150">
-        </a>
-    </x-slot>
-    <!-- Session Status -->
-    <x-auth-session-status class="mb-4" :status="session('status')" />
-
-    <!-- Validation Errors -->
-    <x-auth-validation-errors class="mb-4" :errors="$errors" />
-    <form method="POST" action="{{ route('login') }}">
-
-        <!-- Email Address -->
-        <div class="form-group">
-            <label class="label" for="email">Email</label>
-            <div class="input-group">
-                <input class="form-control" type="email" name="email" value="{{ old('email') }}"
-                    required autofocus>
-                <div class="input-group-append">
-                    <span class="input-group-text">
-                        <i class="mdi mdi-check-circle-outline"></i>
-                    </span>
-                </div>
-            </div>
-        </div>
-
-        <div class="form-group">
-            <label class="label" for="password">Password</label>
-            <div class="input-group">
-                <input id="password" type="password" class="form-control" placeholder="*********"
-                    type="password" name="password" required autocomplete="current-password">
-                <div class="input-group-append">
-                    <span class="input-group-text">
-                        <i class="mdi mdi-check-circle-outline"></i>
-                    </span>
-                </div>
-            </div>
-        </div>
-
-        <!-- Remember Me -->
-        <div class="form-group d-flex justify-content-between">
-            <div class="form-check form-check-flat mt-0">
-                <label class="form-check-label">
-                    <input type="checkbox" id="remember_me" name="remember" class="form-check-input"
-                        checked> {{ __('Remember me') }}</label>
-            </div>
-
-        </div>
-        <div class="form-group">
-            <button class="btn btn-primary submit-btn btn-block"> {{ __('Log in') }}</button>
-        </div>
-
-
-        <div class="wrapper mt-5 text-gray">
-            <p class="footer-text">Copyright © 2022 Digital.AO. Todos Direitos Reservados.</p>
-            <ul class="auth-footer text-gray">
-            </ul>
-        </div>
-    </form>
-</div> --}}
